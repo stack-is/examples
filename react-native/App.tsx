ba83457ca377cdfb72a0afb21ea6646683b8ead8
Tómas Pálsson <tp@joineverything.com>
2023-03-22 14:08:32 +0000

    Added StandardNavigationApp to App.tsx

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AsyncNavigationApp } from './src/async-navigation/AsyncNavigationApp';
+import { StandardNavigationApp } from './src/async-navigation/StandardNavigationApp';
 
 export const App: React.FC = () => {
+    // return <StandardNavigationApp />;
     return <AsyncNavigationApp />;
 };
